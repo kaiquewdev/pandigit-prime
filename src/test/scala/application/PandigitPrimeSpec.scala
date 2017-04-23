27e1fc86bdbcc4a8bffdeffbb6f3dc8dcd0e9a68
Kaique da Silva <kaique.developer@gmail.com>
2017-04-23 05:47:42 -0300

    Testing the instance of numbers sequence

--- src/test/scala/application/PandigitPrimeSpec.scala
+++ src/test/scala/application/PandigitPrimeSpec.scala
@@ -6,4 +6,8 @@ class PandigitalPrimeSpec extends FlatSpec with Matchers {
   "The conditional logic object" should "have a boolean prime comparison method" in {
     PandigitalPrime.isPrime(8988) shouldEqual true 
   }
+
+  "The conditional logic object" should "have a numbersSequence for iterations" in {
+    assume(PandigitalPrime.numbersSequence.isInstanceOf[Range])
+  }
 }
